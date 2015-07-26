810c0079a28cc8c939bfb666fbab3af120d919ff
Deepak Chauhan <foreverdeepak@gmail.com>
2015-07-27 01:04:08 +0530

    mail

--- new/emicare.js
+++ new/emicare.js
@@ -1,6 +1,19 @@
-window.onload = function () {
+function sendMail() {
+    var eminame = document.getElementById("eminame").value;
+    var emiemail = document.getElementById("emiemail").value;
+    var emimobile = document.getElementById("emimobile").value;
+    var emimessage = document.getElementById("emimessage").value;
 
-    var sliderContent = "";
+    var jsonpElem = document.createElement("script");
+    jsonpElem.setAttribute("src", "http://emicare-mailservice.rhcloud.com/sendmail?eminame=" + eminame + "&emiemail=" + emiemail + "&emimobile=" +emimobile + "&emimessage=" + emimessage )
 
+    document.head.appendChild(jsonpElem);
+}
 
+function mailSent() {
+    alert("You query has been sent succesfully.");
+}
+
+function mailError() {
+    alert("Error Sending mail. Please check all fields and try again.");
 }
